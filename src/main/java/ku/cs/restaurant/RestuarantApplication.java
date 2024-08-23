182c8f7da521133b40044d8248da003eca424864
KAwfang <jitlada.yt@gmail.com>
2024-08-23 23:00:02 +0700

    first commit

---
+++ src/main/java/ku/cs/restaurant/RestuarantApplication.java
@@ -0,0 +1,15 @@
+package ku.cs.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author 6510405377 Jitlada Yotinta
+ */
+@SpringBootApplication
+public class RestuarantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestuarantApplication.class, args);
+    }
+
+}
